b2c722f1e674439cfc3a08779437f7684218a067
mooyeon-seo <seomooyeon@gmail.com>
2025-01-16 19:52:46 +0900

    feat: add index file to export pathfinding and maze generation algorithms

---
+++ src/lib/algorithms/index.ts
@@ -0,0 +1,19 @@
+import { dfs } from "./dfs";
+import { bfs } from "./bfs";
+import { dijkstra } from "./dijkstra";
+import { wallFollower } from "./wallFollower";
+import { recursiveBacktracking } from "./recursiveBacktracking";
+import { bidirectional } from "./bidirectional";
+import { astar } from "./astar";
+import { generateMaze } from "./generateMaze";
+
+export const algorithms = {
+  dfs,
+  bfs,
+  dijkstra,
+  wallFollower,
+  recursiveBacktracking,
+  bidirectional,
+  generateMaze,
+  astar,
+};
